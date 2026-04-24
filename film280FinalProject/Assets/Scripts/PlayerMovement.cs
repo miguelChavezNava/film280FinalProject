1d63b67fd385b41b4b592386353f1066292511e5
miguelChavezNava <miguel.271009@nv.ccsd.net>
2026-04-23 23:28:32 -0400

    Initial Project and Player Movement
    I created an initial Unity Project in the Repo and simple WASD player movement for the game.

---
+++ film280FinalProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput.normalized * moveSpeed;
+    }
+}
